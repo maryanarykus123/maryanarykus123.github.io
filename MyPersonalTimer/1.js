0e7af15ce4801d4bd0b75fc30b8bc64990803634
Maryana Rykus <maryanarukyss@gmail.com>
2020-04-30 21:35:48 +0300

    Final

--- MyPersonalTimer/1.js
+++ MyPersonalTimer/1.js
@@ -1,4 +1,5 @@
 btnStart.addEventListener("click", Timer, false);
+btnClear.addEventListener("click", Stop, false);
 var counterSec = 0;
 counterMin = 1;
 counterHour = 1;
@@ -8,13 +9,19 @@ function Timer () {
     document.getElementById("countSec").innerHTML= counterSec;
     setTimeout("Timer()", 1000);
     if (counterSec == 60){
+        document.getElementById("countSec").innerHTML=0;
         document.getElementById("countMin").innerHTML= counterMin;
         counterMin++;
-        return counterSec=0;
-     }
-     if ( counterMin== 60){
+        return counterSec = 0;
+    }
+
+    if ( counterMin== 60){
+        document.getElementById("countMin").innerHTML=0;
         document.getElementById("countHours").innerHTML= counterHour;
         counterHour++;
          return counterMin = 0;
      }
+}
+ function Stop(){
+    pause;
 }
