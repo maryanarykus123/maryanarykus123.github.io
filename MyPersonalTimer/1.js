8c1d4b41cf0d773cfd1437fa4be70f4124e9164c
Maryana Rykus <maryanarukyss@gmail.com>
2020-05-01 13:13:34 +0300

    WithButton

--- MyPersonalTimer/1.js
+++ MyPersonalTimer/1.js
@@ -1,27 +1,43 @@
 btnStart.addEventListener("click", Timer, false);
-btnClear.addEventListener("click", Stop, false);
+btnRestart.addEventListener("click", RestartTimer, false);
 var counterSec = 0;
-counterMin = 1;
-counterHour = 1;
+counterMin = 0;
+counterHour = 0;
+
 
 function Timer () {
     counterSec++;
     document.getElementById("countSec").innerHTML= counterSec;
     setTimeout("Timer()", 1000);
     if (counterSec == 60){
+        counterMin++;
         document.getElementById("countSec").innerHTML=0;
         document.getElementById("countMin").innerHTML= counterMin;
-        counterMin++;
         return counterSec = 0;
     }
 
     if ( counterMin== 60){
+        counterHour++;
         document.getElementById("countMin").innerHTML=0;
         document.getElementById("countHours").innerHTML= counterHour;
-        counterHour++;
          return counterMin = 0;
      }
-}
- function Stop(){
-    pause;
+    
+     
+    }
+ function RestartTimer(){
+     if(counterSec != 0 || counterMin  != 0 || counterHour != 0 ){
+        document.getElementById("countSec").innerHTML=0;
+        document.getElementById("countMin").innerHTML=0;
+        document.getElementById("countHours").innerHTML=0;
+        counterSec = 0;
+       return;
+     }
+ }
+ btnStop.addEventListener("click", StopTimer, false);
+
+ function StopTimer(e) {
+    if (confirm("Ви зупинили таймер \n" +"Години:" + counterHour +"  Хвилини:"+ counterMin + "  Секунди:" + counterSec )) {
+        return;
+    }
 }
