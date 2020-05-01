1443a4ef05d69ec9f94a595f64bc50d74b96060e
Maryana Rykus <maryanarukyss@gmail.com>
2020-05-01 18:03:33 +0300

    WithButton

--- MyPersonalTimer/1.js
+++ MyPersonalTimer/1.js
@@ -5,39 +5,36 @@ counterMin = 0;
 counterHour = 0;
 
 
-function Timer () {
+function Timer() {
     counterSec++;
-    document.getElementById("countSec").innerHTML= counterSec;
+    document.getElementById("countSec").innerHTML = counterSec;
     setTimeout("Timer()", 1000);
-    if (counterSec == 60){
+    if (counterSec == 60) {
         counterMin++;
-        document.getElementById("countSec").innerHTML=0;
-        document.getElementById("countMin").innerHTML= counterMin;
+        document.getElementById("countSec").innerHTML = 0;
+        document.getElementById("countMin").innerHTML = counterMin;
         return counterSec = 0;
     }
 
-    if ( counterMin== 60){
+    if (counterMin == 60) {
         counterHour++;
-        document.getElementById("countMin").innerHTML=0;
-        document.getElementById("countHours").innerHTML= counterHour;
-         return counterMin = 0;
-     }
-    
-     
+        document.getElementById("countMin").innerHTML = 0;
+        document.getElementById("countHours").innerHTML = counterHour;
+        return counterMin = 0;
     }
- function RestartTimer(){
-     if(counterSec != 0 || counterMin  != 0 || counterHour != 0 ){
-        document.getElementById("countSec").innerHTML=0;
-        document.getElementById("countMin").innerHTML=0;
-        document.getElementById("countHours").innerHTML=0;
-        counterSec = 0;
-       return;
-     }
- }
- btnStop.addEventListener("click", StopTimer, false);
 
- function StopTimer(e) {
-    if (confirm("Ви зупинили таймер \n" +"Години:" + counterHour +"  Хвилини:"+ counterMin + "  Секунди:" + counterSec )) {
+
+}
+function RestartTimer() {
+    if (counterSec != 0 || counterMin != 0 || counterHour != 0) {
+        location.reload();
+        return;
+    }
+}
+btnStop.addEventListener("click", StopTimer, false);
+
+function StopTimer(e) {
+    if (confirm("Ви зупинили таймер \n" + "Години:" + counterHour + "  Хвилини:" + counterMin + "  Секунди:" + counterSec)) {
         return;
     }
 }
